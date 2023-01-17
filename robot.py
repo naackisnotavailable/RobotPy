31918c7b21043c21a4fdb80684ab60092a49aaa6
naackisnotavailable <108686834+naackisnotavailable@users.noreply.github.com>
2023-01-16 22:24:37 -0500

    hi

---
+++ robot.py
@@ -0,0 +1,29 @@
+import wpilib
+import wpilib.drive
+from wpilib.drive import DifferentialDrive
+import rev
+
+#  table = networktables.NetworkTables.getTable("limelight")
+#  tx = table.getNumber('tx',None)
+#  ty = table.getNumber('ty',None)
+#  ta = table.getNumber('ta',None)
+#  ts = table.getNumber('ts',None)
+
+class Robot(wpilib.TimedRobot):
+    def robotInit(self):
+        self.leftMotor = rev.CANSparkMax(2, rev.CANSparkMax.MotorType.kBrushless)
+
+        #self.driveTrain = DifferentialDrive(self.leftMotor, self.rightMotor)
+
+        #self.l_stick = wpilib.Joystick(0)
+        #self.r_stick = wpilib.Joystick(1)
+
+    def teleopPeriodic(self):
+        # Create tank drive
+        #self.driveTrain.tankDrive(self.l_stick.getY(), self.r_stick.getY())
+        self.leftMotor.setVoltage(1.20)
+        
+
+
+if __name__ == "__main__":
+    wpilib.run(Robot)
