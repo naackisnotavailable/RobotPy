fb3b126dcad4edd108f1456199be39de48b38ed1
naackisnotavailable <108686834+naackisnotavailable@users.noreply.github.com>
2023-01-19 17:17:34 -0500

    hi

--- robot.py
+++ robot.py
@@ -4,6 +4,7 @@ import wpilib.drive
 import rev
 import ctre
 import time
+#from ctre import _ctre as ct
 #from networktables import NetworkTable
 #import cv2
 #import networktables.networktable
@@ -22,14 +23,18 @@ from ntcore import _ntcore
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
-        time.sleep(5.0)
-        #NetworkTables.initialize(server='10.55.30.2')
-        self.leftMotor = rev.CANSparkMax(22, rev.CANSparkMax.MotorType.kBrushless)
-        #ctre.TalonFXControlMode.PercentOutput
-        self.rightMotor = ctre.TalonFX(5, None)
-        self.rightMotor.configAllSettings(ctre._ctre.TalonFXConfiguration, 0)
-        #self.driveTrain = DifferentialDrive(self.leftMotor, self.rightMotor)
+        #time.sleep(5.0)
 
+        #NetworkTables.initialize(server='10.55.30.2')
+        self.rightMotor1 = ctre._ctre.TalonFX(5)
+        self.rightMotor2 = ctre._ctre.TalonFX(6)
+        self.leftMotor1 = ctre._ctre.TalonFX(7)
+        self.leftMotor2 = ctre._ctre.TalonFX(8)
+        #self.rightMotor.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
+        #self.rightMotor.configAllSettings(ct.TalonFX, ct.TalonFXConfiguration, 50)
+        #self.driveTrain = DifferentialDrive(self.leftMotor, self.rightMotor)
+        self.leftMotor = rev.CANSparkMax(22, rev.CANSparkMax.MotorType.kBrushless)
+        #self.rightMotor = ctre.TalonFX(5)
         #wpilib.CameraServer.launch()
 
         #self.l_stick = wpilib.Joystick(0)
@@ -37,13 +42,19 @@ class Robot(wpilib.TimedRobot):
 
     def teleopPeriodic(self):
         #table = NetworkTables.getTable("limelight")
-        self.rightMotor.set(ctre._ctre.TalonFX, ctre._ctre.TalonFXControlMode.PercentOutput, float(0.5))
-        inst = _ntcore.NetworkTableInstance.getDefault()
-        table = inst.getTable("limelight")
-        table.putNumber('ledmode', 3)
-        table.putNumber('camMode', 0)
-        tv = table.getNumber('tv', None)
-        
+        #self.rightMotor.set(ctre._ctre.TalonFXControlMode.PercentOutput, float(0.5))
+        self.rightMotor1.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
+        self.rightMotor2.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
+        self.leftMotor1.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
+        self.leftMotor2.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
+
+
+        #inst = _ntcore.NetworkTableInstance.getDefault()
+        #table = inst.getTable("limelight")
+        #table.putNumber('ledmode', 3)
+        #table.putNumber('camMode', 0)
+        #tv = table.getNumber('tv', None)
+        #self.leftMotor.setVoltage(1.0)
         #table.putNumber('ledMode', 3)
         #table.putNumber('camMode', 0)
         #tv = table.getNumber('tv', None)
@@ -51,16 +62,18 @@ class Robot(wpilib.TimedRobot):
         # Create tank drive
         #self.driveTrain.tankDrive(self.l_stick.getY(), self.r_stick.getY())
         t = 0
-        self.leftMotor.setVoltage(1.0)
-        while t<= 2:
-            t+=0.1
-            time.sleep(0.2)
-        self.leftMotor.setVoltage(0.0)
-
-        if tv == 1:
-            self.leftMotor.setVoltage(1.0)
-        elif tv == 0:
-            self.leftMotor.setVoltage(0.0)
+        self.leftMotor.set(0.1)
+        
+        #while t<= 2:
+        #    t+=0.1
+        #    time.sleep(0.2)
+        #self.leftMotor.setVoltage(0.0)
+        #while True:
+        #    if tv == 1:
+        #        self.leftMotor.setVoltage(1.0)
+        #    elif tv == 0:
+        #        self.leftMotor.setVoltage(0.0)
+        #    time.sleep(1)
         
 
 
