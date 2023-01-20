0ae0267fd3c10f296bb1dd1ad648ed5b7e955b44
naackisnotavailable <108686834+naackisnotavailable@users.noreply.github.com>
2023-01-19 22:02:43 -0500

    lots of limelight stuff, NOT FUNCTIONING

--- robot.py
+++ robot.py
@@ -11,18 +11,18 @@ import navx
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
         #time.sleep(5.0)
-        navx.AHRS.__init__(wpilib._wpilib.SPI.Port.kMXP, 500000, 60)  # try .createspi if it doesnt work
+        #navx.AHRS.__init__(wpilib._wpilib.SPI.Port.kMXP, 500000, 60)  # try .createspi if it doesnt work
         # https://robotpy.readthedocs.io/projects/navx/en/stable/api.html
 
-        self.Talon1 = ctre._ctre.TalonFX(5)
-        self.Talon2 = ctre._ctre.TalonFX(6)
-        self.Talon3 = ctre._ctre.TalonFX(7)
-        self.Talon4 = ctre._ctre.TalonFX(8)
+        #self.Talon1 = ctre._ctre.TalonFX(5)
+        #self.Talon2 = ctre._ctre.TalonFX(6)
+        #self.Talon3 = ctre._ctre.TalonFX(7)
+        #self.Talon4 = ctre._ctre.TalonFX(8)
 
-        self.Talon1.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
-        self.Talon2.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
-        self.Talon3.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
-        self.Talon4.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
+        #self.Talon1.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
+        #self.Talon2.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
+        #self.Talon3.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
+        #self.Talon4.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
 
         self.leftMotor = rev.CANSparkMax(22, rev.CANSparkMax.MotorType.kBrushless)
 
@@ -32,12 +32,33 @@ class Robot(wpilib.TimedRobot):
     def teleopPeriodic(self):
         #table = NetworkTables.getTable("limelight")
         #self.rightMotor.set(ctre._ctre.TalonFXControlMode.PercentOutput, float(0.5))
-        self.Talon1.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5) 
-        self.Talon2.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
-        self.Talon3.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
-        self.Talon4.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
+        #self.Talon1.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5) 
+        #self.Talon2.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
+        #self.Talon3.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
+        #self.Talon4.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
 
-        self.leftMotor.set(0.1) #Random NEO motor
+        inst = _ntcore.NetworkTableInstance.getDefault()
+        table = inst.getTable("limelight")
+        table.putNumber('ledmode', 3)
+        table.putNumber('camMode', 0)
+        tv = table.getNumber('tv', None)
+        self.leftMotor.setVoltage(1.0)
+        table.putNumber('ledMode', 3)
+        table.putNumber('camMode', 0)
+        tv = table.getNumber('tv', None)
+
+        self.leftMotor.set(0.5) #Random NEO motor
+
+        while t<= 2:
+            t+=0.1
+            time.sleep(0.2)
+        self.leftMotor.setVoltage(0.0)
+        while True:
+            if tv == 1:
+                self.leftMotor.setVoltage(1.0)
+            elif tv == 0:
+                self.leftMotor.setVoltage(0.0)
+            time.sleep(1)
         
 
 
