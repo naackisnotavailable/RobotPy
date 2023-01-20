53d5f2a2da943b33402ae3cd000616acac7e632f
naackisnotavailable <108686834+naackisnotavailable@users.noreply.github.com>
2023-01-19 23:47:10 -0500

    teleop maybe possibly hopefully

--- robot.py
+++ robot.py
@@ -1,65 +1,47 @@
 import wpilib
 import wpilib.drive
-import rev
 import ctre
-import time
+import rev
+#from ntcore import _ntcore
+#import navx
 import wpilib.interfaces
-from ntcore import _ntcore
-import navx
-
-
+#print(str(Ttest.TalonFXConfiguration.motionCruiseVelocity.getter))
+#ctre._ctre.TalonFX.configAllSettings(ctre._ctre.TalonFXConfiguration)
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
         #time.sleep(5.0)
-        #navx.AHRS.__init__(wpilib._wpilib.SPI.Port.kMXP, 500000, 60)  # try .createspi if it doesnt work
+        #navx.AHRS.create_spi(wpilib._wpilib.SPI.Port.kMXP, 500000, 60)  # try .createspi if it doesnt work
         # https://robotpy.readthedocs.io/projects/navx/en/stable/api.html
 
-        #self.Talon1 = ctre._ctre.TalonFX(5)
-        #self.Talon2 = ctre._ctre.TalonFX(6)
-        #self.Talon3 = ctre._ctre.TalonFX(7)
-        #self.Talon4 = ctre._ctre.TalonFX(8)
+        self.Talon1 = ctre._ctre.TalonFX(5)
+        self.Talon2 = ctre._ctre.TalonFX(6)
+        self.Talon3 = ctre._ctre.TalonFX(7)
+        self.Talon4 = ctre._ctre.TalonFX(8)
 
-        #self.Talon1.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
-        #self.Talon2.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
-        #self.Talon3.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
-        #self.Talon4.configAllSettings(ctre._ctre.TalonFXConfiguration, 50)
-
+        self.Talon1.configFactoryDefault()
+        self.Talon2.configFactoryDefault()
+        self.Talon3.configFactoryDefault()
+        self.Talon4.configFactoryDefault()
+        #self.Talon1.configMotionAcceleration(0.5, 0)
         self.leftMotor = rev.CANSparkMax(22, rev.CANSparkMax.MotorType.kBrushless)
 
         #self.l_stick = wpilib.Joystick(0)
         #self.r_stick = wpilib.Joystick(1)
+        self.stick = wpilib.PS4Controller(int) # its something 0-5 lol
 
     def teleopPeriodic(self):
         #table = NetworkTables.getTable("limelight")
         #self.rightMotor.set(ctre._ctre.TalonFXControlMode.PercentOutput, float(0.5))
-        #self.Talon1.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5) 
-        #self.Talon2.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
-        #self.Talon3.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
-        #self.Talon4.set(ctre._ctre.ControlMode.PercentOutput, 0.5, ctre._ctre.DemandType.AuxPID, 0.5)
 
-        inst = _ntcore.NetworkTableInstance.getDefault()
-        table = inst.getTable("limelight")
-        table.putNumber('ledmode', 3)
-        table.putNumber('camMode', 0)
-        tv = table.getNumber('tv', None)
-        self.leftMotor.setVoltage(1.0)
-        table.putNumber('ledMode', 3)
-        table.putNumber('camMode', 0)
-        tv = table.getNumber('tv', None)
+        connectStick = self.stick.isConnected
+        if connectStick == True:
+            self.leftMotor.set(0.5)
+        sped = self.stick.getLeftY
 
-        self.leftMotor.set(0.5) #Random NEO motor
-
-        while t<= 2:
-            t+=0.1
-            time.sleep(0.2)
-        self.leftMotor.setVoltage(0.0)
-        while True:
-            if tv == 1:
-                self.leftMotor.setVoltage(1.0)
-            elif tv == 0:
-                self.leftMotor.setVoltage(0.0)
-            time.sleep(1)
-        
+        self.Talon1.set(ctre._ctre.ControlMode.PercentOutput, sped, ctre._ctre.DemandType.AuxPID, 0.5) 
+        self.Talon2.set(ctre._ctre.ControlMode.PercentOutput, sped, ctre._ctre.DemandType.AuxPID, 0.5)
+        self.Talon3.set(ctre._ctre.ControlMode.PercentOutput, sped, ctre._ctre.DemandType.AuxPID, 0.5)
+        self.Talon4.set(ctre._ctre.ControlMode.PercentOutput, sped, ctre._ctre.DemandType.AuxPID, 0.5)
 
 
 if __name__ == "__main__":
