befc71a0e3ba69918d5920e0bbe70e6cf3e1fc69
naackisnotavailable <108686834+naackisnotavailable@users.noreply.github.com>
2023-01-20 19:38:29 -0500

    tele stuff with talons (fully working)

--- robot.py
+++ robot.py
@@ -14,27 +14,32 @@ class Robot(wpilib.TimedRobot):
         #self.Talon1.configMotionAcceleration(0.5, 0)
 
         self.neo1 = rev.CANSparkMax(14, rev.CANSparkMax.MotorType.kBrushless)
-
+        self.leftTalon1 = ctre.TalonFX(5)
+        self.leftTalon2 = ctre.TalonFX(6)
+        self.rightTalon1 = ctre.TalonFX(7)
+        self.rightTalon2 = ctre.TalonFX(8)
+        self.leftTalon1.configFactoryDefault()
+        self.leftTalon2.configFactoryDefault()
+        self.rightTalon1.configFactoryDefault()
+        self.rightTalon2.configFactoryDefault()
+        #self.leftTalon2.setInverted(True)
         #self.l_stick = wpilib.Joystick(0)
         #self.r_stick = wpilib.Joystick(1)
         self.stick = wpilib.XboxController(0) # its something 0-5 lol
 
     def teleopPeriodic(self):
-        inst = _ntcore.NetworkTableInstance.getDefault()
-        table = inst.getTable("limelight")
+        sped = self.stick.getLeftY()
+        if abs(sped) >= 0.05:
+            self.leftTalon1.set(ctre.ControlMode.PercentOutput, sped)
+            self.leftTalon2.set(ctre.ControlMode.PercentOutput, sped)
+            self.rightTalon1.set(ctre.ControlMode.PercentOutput, sped)
+            self.rightTalon2.set(ctre.ControlMode.PercentOutput, sped)
+        elif abs(sped) <= 0.2:
+            self.leftTalon1.set(ctre.ControlMode.PercentOutput, 0.0)
+            self.leftTalon2.set(ctre.ControlMode.PercentOutput, 0.0)
+            self.rightTalon1.set(ctre.ControlMode.PercentOutput, 0.0)
+            self.rightTalon2.set(ctre.ControlMode.PercentOutput, 0.0)
 
-        table.putNumber('ledMode', 3)
-        table.putNumber('camMode', 0)
-        tv = table.getNumber('tv', None)
-        tx = table.getNumber('tx', None)
-        ty = table.getNumber('ty', None)
-        ta = table.getNumber('ta', None)
-
-        if tv == True:
-            self.neo1.set(0.5)
-        if tv == False:
-            self.neo1.set(0.0)
 
 if __name__ == "__main__":
     wpilib.run(Robot)
-
